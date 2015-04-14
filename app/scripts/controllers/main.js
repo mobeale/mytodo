5577dc2ba288586bd2dbe45ce124d1eb00a65ddb
Mathew <M.beale@me.com>
2015-04-14 22:28:06 +0100

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,69 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mytodoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mytodoApp
+ */
+
+angular.module('mytodoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.todos = [];
+    $scope.addTodo = function () {
+      $scope.todos.push($scope.todo);
+      $scope.todo = '';
+    };
+
+    $scope.removeTodo = function (index) {
+      $scope.todos.splice(index, 1);
+
+    };
+
+  });
+
+angular.module('mytodoApp').controller('ModalDemoCtrl', function ($scope, $modal, $log) {
+
+  $scope.items = ['item1', 'item2', 'item3'];
+
+  $scope.open = function (size) {
+
+    var modalInstance = $modal.open({
+      templateUrl: 'myModalContent.html',
+      backdrop: false,
+      controller: 'ModalInstanceCtrl',
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+});
+
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $modal service used above.
+
+angular.module('mytodoApp').controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+});
